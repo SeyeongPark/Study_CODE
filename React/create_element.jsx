e07dbf798af656a9de964074416c37e2248e2e56
SeyeongPark <lynn.yeong.102@gmail.com>
2022-06-09 17:46:22 -0400

    Create create_element.jsx

---
+++ React/create_element.jsx
@@ -0,0 +1,13 @@
+const element = (
+  <h1 className = "greeting> 
+    Hello, world!
+  </h1>
+  );
+
+const element = React.createElement(
+   'h1',
+  {clssName: "greeting"},
+  'Hello, World'
+  )
+
+// https://www.youtube.com/watch?v=fimT15Iu7jg
