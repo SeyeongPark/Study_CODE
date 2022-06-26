91b68eb9a4fc1af781c04ec59d7a24817b608f32
SeyeongPark <lynn.yeong.102@gmail.com>
2022-06-25 21:24:23 -0400

    list/array

---
+++ Python/list/array.py
@@ -0,0 +1,10 @@
+import numbers
+
+
+numbers = [1, 2, 3, 4, 5]
+numbers.insert(0, 6)
+print(numbers) # [6, 1, 2, 3, 4, 5]
+
+print(1 in numbers) # True
+
+print(len(numbers)) # 6
---
+++ Python/list/list.py
@@ -0,0 +1,6 @@
+names = ["May", "John", "Bob", "Sam", "Tom"]
+print(names[-1]) # Tom
+names[-1] = "Tam" # change to Tam
+print(names[-1]) # Tam
+
+print(names[2:4]) # ['Bob', 'Sam']
