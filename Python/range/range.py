d2619b718b0392e51f9e85c8df2c4dd51f6faef6
SeyeongPark <lynn.yeong.102@gmail.com>
2022-06-25 21:30:51 -0400

    range

---
+++ Python/range/range.py
@@ -0,0 +1,25 @@
+numbers = range(5)
+    # 0
+    # 1
+    # 2
+    # 3
+    # 4
+
+numbers = range(5, 10)
+    # 5
+    # 6
+    # 7
+    # 8
+    # 9
+
+for number in numbers:
+    print(number)
+
+
+for number in range(5):
+    print(number)
+    # 0
+    # 1
+    # 2
+    # 3
+    # 4
